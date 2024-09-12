d7a672d934a2d05c877f32ff92d4417959fcfe6c
caitlin-dalwai <dalwaicaitlin@gmail.com>
2024-09-12 16:15:59 +0200

    this is the actually the chosen one

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api/users', userRoutes);
-app.use('/api/bookings', bookingRoutes);
+app.use('/users', userRoutes);
+app.use('/bookings', bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
